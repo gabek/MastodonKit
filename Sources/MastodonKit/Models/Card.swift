0d249385fab263d88aacb79ec832465224253b72
Gabe Kangas <gabek@real-ity.com>
2018-11-20 23:41:29 -0800

    Extend card

--- Sources/MastodonKit/Models/Card.swift
+++ Sources/MastodonKit/Models/Card.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 public struct Card {
+    public let type: String
     /// The url associated with the card.
     public let url: URL
     /// The title of the card.
@@ -9,11 +10,20 @@ public struct Card {
     public let description: String
     /// The image associated with the card, if any.
     public let image: URL?
+    public let authorName: String?
+    public let authorUrl: String?
+    public let providerName: String?
+    public let providerUrl: String?
+    public let html: String?
+    public let width: Int?
+    public let height: Int?
+    
 }
 
 extension Card {
     init?(from dictionary: JSONDictionary) {
         guard
+            let type = dictionary["type"] as? String,
             let urlString = dictionary["url"] as? String,
             let url = URL(string: urlString),
             let title = dictionary["title"] as? String,
@@ -22,9 +32,17 @@ extension Card {
                 return nil
         }
 
+        self.type = type
         self.url = url
         self.title = title
         self.description = description
         self.image = dictionary["image"].flatMap(asString).flatMap(URL.init)
+        self.authorName = dictionary["author_name"] as? String
+        self.authorUrl = dictionary["author_url"] as? String
+        self.providerName = dictionary["provider_name"] as? String
+        self.providerUrl = dictionary["provider_url"] as? String
+        self.html = dictionary["html"] as? String
+        self.width = dictionary["width"] as? Int
+        self.height = dictionary["height"] as? Int
     }
 }
